3cb7cb0a1fbd9c26abd1282475d32f0e73ecd86a
Jacek <jacek@ivolution.pl>
2015-02-11 15:06:29 +0100

    diet images

--- ydcommon/file_system_storage.py
+++ ydcommon/file_system_storage.py
@@ -1,5 +1,6 @@
 from django.contrib.staticfiles.storage import StaticFilesStorage
 from django.conf import settings
+from django.core.management import call_command
 
 
 class YDcommonFileSystemStorage(StaticFilesStorage):
@@ -7,3 +8,4 @@ class YDcommonFileSystemStorage(StaticFilesStorage):
     def post_process(self, *args, **kwargs):
         if 'diet-images' in settings.INSTALLED_APPS:
             print "diet available"
+            call_command('diet_images', settings.STATIC_ROOT)
